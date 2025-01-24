47c3b32412967b3caa2f40a1eacc62b4561bfd6c
noway777 <ahmadrezamohammadi777@gmail.com>
2025-01-24 15:57:18 +0330

    feat: footer added

--- src/components/home/footer/footer.jsx
+++ src/components/home/footer/footer.jsx
@@ -1,3 +1,15 @@
-export default function Footer() {
-  return;
+import React from "react";
+import "./footer.css";
+import FooterLogo from "./footerLogo/footerLogo";
+import FooterOptions from "./footerOptions/footerOptions";
+
+function Footer() {
+  return (
+    <div className="footer-container">
+      <FooterLogo />
+      <FooterOptions />
+    </div>
+  );
 }
+
+export default Footer;
---
+++ src/components/home/footer/footerLogo/footerLogo.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function FooterLogo() {
+  return (
+    <div>
+      
+      <div className="left-side-footer">
+        <img
+          className="footer-img"
+          src="https://www.svgrepo.com/show/373541/cuda.svg"
+          alt="Logo"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default FooterLogo;
