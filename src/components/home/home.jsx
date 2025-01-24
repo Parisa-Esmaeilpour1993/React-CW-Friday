e4a53b33f8cc54f751d58af109b09b9b657f898d
Parisa Esmaeilpour <esmaeilpour.parisa@gmail.com>
2025-01-24 14:57:24 +0330

    feat: create base of project

---
+++ src/components/home/home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./header/header";
+import Main from "./main/main";
+import Footer from "./footer/footer";
+
+export default function HomeComponent() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,5 @@
+import HomeComponent from "../../components/home/home";
+
+export default function HomePage() {
+  return <HomeComponent />;
+}
